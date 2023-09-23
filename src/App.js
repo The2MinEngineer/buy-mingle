c0405f6fb57b374544a305025b323d360de7f36a
pheany <114763753+pheany@users.noreply.github.com>
2023-09-23 05:53:01 +0100

    update menu-item to navigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
